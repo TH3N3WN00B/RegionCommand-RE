888f5f0ea3577afa35782b135b96ecc4439b4cb4
Rigorberto <na@na>
2026-08-26 10:40:33 -0600

    Update version to 2.1.1 to match tag

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "fr.klemms.regioncommand"
-version = "2.0.1"
+version = "2.1.1"
 description = "Execute commands when players enter or leave WorldGuard regions"
 
 repositories {
